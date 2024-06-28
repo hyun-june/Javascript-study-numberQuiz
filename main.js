9b7edf93b129a7fe4e9e222a53cc753fc72c5a26
Hyunjun Lee <leehyunjune11@gmail.com>
2024-06-28 20:57:34 +0900

    js update

--- main.js
+++ main.js
@@ -8,3 +8,67 @@
 // 유저가 1~100 범위 밖의 숫자를 입력하면 알려주고, 기회를 깎지 않는다.
 // 유저가 이미 입력한 숫자를 또 입력하면, 알려주고, 기회를 깎지 않는다.
 
+let randomNum = 0;
+let userInput = document.getElementById("user-input");
+let playButton = document.getElementById("play-button");
+let resetButton = document.getElementById("reset-button");
+let resultArea = document.getElementById("result-area");
+let chanceArea = document.getElementById("chance-area");
+let historyArea = document.getElementById("history-area")
+let chances = 5;
+let history = [];
+
+const randomNumber = () =>{
+    randomNum = Math.floor(Math.random()*100+1);
+    console.log("정답 :",randomNum)
+}
+
+const play = () =>{
+    userValue = userInput.value;
+
+    if(history.includes(userValue)){
+        resultArea.textContent = "다른 숫자를 입력해주세요."
+        return;
+    }
+
+    if(userValue < 1 || userValue > 100){
+        resultArea.textContent = "1~100 사이의 숫자를 입력해주세요."
+        return;
+    }
+
+    chances -- ;
+    chanceArea.textContent = `남은 기회 : ${chances}번`
+
+    history.push(userValue);
+    historyArea.textContent = `입력한 숫자 : ${history}`
+
+    if(userValue < randomNum){
+        resultArea.textContent = "Up"
+    } else if(userValue > randomNum){
+        resultArea.textContent = "Down"
+    } else if(userValue == randomNum){
+        resultArea.textContent = "정답"
+    }
+
+    if(chances == 0){
+        playButton.disabled = true;
+    }
+}
+
+const reset = () =>{
+    chances = 5;
+    history = [];
+    chanceArea.textContent = `남은 기회 : ${chances}번`
+    historyArea.textContent = `입력한 숫자 : ${history}`
+    playButton.disabled = false;
+    userInput.value = "";
+    randomNumber();
+}
+
+playButton.addEventListener("click",play);
+resetButton.addEventListener("click",reset);
+userInput.addEventListener("focus",()=>{
+    userInput.value = "";
+})
+
+randomNumber();
